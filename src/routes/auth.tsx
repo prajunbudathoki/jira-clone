a10bedc4a376b2925bfaa2ddd93724a31173dd10
prajun <prajunbudathoki1@gmail.com>
2025-10-17 09:42:20 +0545

    feat: add toggle link between sign-in and sign-up pages (dynamically)

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,17 +1,33 @@
 import { Button } from "@/components/ui/button";
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import {
+  createFileRoute,
+  Link,
+  Outlet,
+  useLocation,
+} from "@tanstack/react-router";
 
 export const Route = createFileRoute("/auth")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const location = useLocation();
   return (
     <main className="bg-neutral-100  min-h-screen">
       <div className="mx-auto max-w-screen-xl p-4">
         <nav className="flex justify-between items-center">
           <p>logo</p>
-          <Button variant={"secondary"}>Sign Up</Button>
+          <Button asChild variant={"secondary"}>
+            <Link
+              to={
+                location.pathname === "/auth/sign-in"
+                  ? "/auth/sign-up"
+                  : "/auth/sign-in"
+              }
+            >
+              {location.pathname === "/auth/sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
+          </Button>
         </nav>
         <div className="flex justify-center mt-5">
           <Outlet />
